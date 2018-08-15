7f8b9d537aeb3014dc9374bfff703b2930dd704e
Daniel Gruber <mail@danielgruber.com>
2018-08-15 17:39:59 +0200

    fixed scripts

--- pks/install_go.sh
+++ pks/install_go.sh
@@ -2,5 +2,5 @@
 wget https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.10.3.linux-amd64.tar.gz
 echo "export GOPATH=$HOME/go" >> .profile
-echo "export PATH=$PATH:$GOPATH/bin:/usr/local/bin/go" >> .profile
+echo "export PATH=$PATH:$GOPATH/bin:/usr/local/go/bin" >> .profile
 
